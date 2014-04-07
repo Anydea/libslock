7cad740107e6a2daf1b665b9c39bcdd73861dd8a
anydea <anydea@anydea.(none)>
2014-04-08 01:17:45 +0200

    Stress test script

--- Barriers/Test.sh
+++ Barriers/Test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for n in {20..200..2}
+for n in {20..200..20}
 	do 
 		echo $n
-		for r in {10..100..2}
+		for r in {10..100..10}
 			do	
 				./stress_test.o -r $r -n $n >> StaticTreeB.txt
 			done
