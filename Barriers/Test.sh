d9c780063222e1110a4db90aeee8328134fef569
Chengzhen Wu <chwu@lpdxeon2680.epfl.ch>
2014-05-12 16:58:39 +0200

    .dat update

--- Barriers/Test.sh
+++ Barriers/Test.sh
@@ -4,52 +4,52 @@ rm Result.txt
 sample=$(seq $2 $3 $4)
 
 make Sense
-echo SenseBarrier>>Result.txt
+#echo SenseBarrier>>Result.txt
 for n in $sample
 	do 
 		echo $n 
-			./Sense  -n $n>>Result.txt
+			./Sense  -n $n>SeB.dat
 	done
 
 make MSense
-echo SenseBarrier with less cores>>Result.txt
+#echo SenseBarrier with less cores>>Result.txt
 for n in $sample
 	do 
 		echo $n 
-			./MSense  -n $n>>Result.txt
+			./MSense  -n $n>>MSeB.dat
 	done
 
 make Tree
-echo TreeBarrier>>Result.txt
+#echo TreeBarrier>>Result.txt
 for n in $sample
 	do 
 		echo $n 
-			./Tree  -n $n -f $1>>Result.txt
+			./Tree  -n $n -f $1>>TB.dat
 	done
 
 make MTree
-echo TreeBarrier with less cores>>Result.txt
+#echo TreeBarrier with less cores>>Result.txt
 for n in $sample
 	do 
 		echo $n 
-			./MTree  -n $n -f $1>>Result.txt
+			./MTree  -n $n -f $1>>MTB.dat
 	done
 
 
 make Static
-echo StaticTreeBarrier>>Result.txt
+#echo StaticTreeBarrier>>Result.txt
 for n in $sample
 	do 
 		echo $n 
-			./Static  -n $n -f $1>>Result.txt
+			./Static  -n $n -f $1>>StB.dat
 	done
 
 make MStatic 
-echo StaticTreeBarrier with less cores>>Result.txt
+#echo StaticTreeBarrier with less cores>>Result.txt
 for n in $sample
 	do 
 		echo $n 
-			./MStatic -n $n -f $1>>Result.txt
+			./MStatic -n $n -f $1>>MStB.dat
 	done
 
 make clean
