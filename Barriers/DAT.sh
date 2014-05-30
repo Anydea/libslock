2f92efd9134fafe94f50977d33e411df4d6d8af0
Chengzhen Wu <chwu@lpdxeon2680.epfl.ch>
2014-05-30 03:28:33 +0200

    LOOP test DAT.sh

--- Barriers/DAT.sh
+++ Barriers/DAT.sh
@@ -1,16 +1,60 @@
 #!/bin/bash
-rm L*.dat
+rm *.dat
 sample=$(seq $2 $3 $4)
 LOOP=$(seq 1 10)
+
+make Sense
+make MSense
+make Tree
+make MTree
+make Static
+make MStatic 
+
 make LSense
 make LMSense
 make LTree
 make LMTree
 make LStatic
 make LMStatic 
-
 for loop in $LOOP
 	do
+	echo Sense
+	for n in $sample
+		do 
+			echo $n 
+				./Sense  -n $n>>SeB$loop.dat
+		done
+	echo Msense
+	for n in $sample
+		do 
+			echo $n 
+				./MSense  -n $n>>MSeB$loop.dat
+		done
+	echo Tree
+	for n in $sample
+		do 
+			echo $n 
+				./Tree  -n $n -f $1>>TB$loop.dat
+		done
+	echo MTree
+	for n in $sample
+		do 
+			echo $n 
+				./MTree  -n $n -f $1>>MTB$loop.dat
+		done
+	echo Static
+	for n in $sample
+		do 
+			echo $n 
+				./Static  -n $n -f $1>>StB$loop.dat
+		done
+	echo MStatic
+	for n in $sample
+		do 
+			echo $n 
+				./MStatic -n $n -f $1>>MStB$loop.dat
+		done
+
 	echo Sense
 	for n in $sample
 		do 
@@ -50,7 +94,6 @@ for loop in $LOOP
 		done
 
 	done
-#gunplot latencyplot
 make clean
 
 
--- Barriers/Test.sh
+++ Barriers/Test.sh
@@ -1,60 +1,60 @@
 #!/bin/bash
-rm Result.txt
 rm *.dat
 sample=$(seq $2 $3 $4)
+LOOP=$(seq 1 10)
 
 make Sense
-#echo SenseBarrier>>Result.txt
-for n in $sample
-	do 
-		echo $n 
-			./Sense  -n $n>>SeB.dat
-	done
-
 make MSense
-#echo SenseBarrier with less cores>>Result.txt
-for n in $sample
-	do 
-		echo $n 
-			./MSense  -n $n>>MSeB.dat
-	done
-
 make Tree
-#echo TreeBarrier>>Result.txt
-for n in $sample
-	do 
-		echo $n 
-			./Tree  -n $n -f $1>>TB.dat
-	done
-
 make MTree
-#echo TreeBarrier with less cores>>Result.txt
-for n in $sample
-	do 
-		echo $n 
-			./MTree  -n $n -f $1>>MTB.dat
-	done
-
-
 make Static
-#echo StaticTreeBarrier>>Result.txt
-for n in $sample
-	do 
-		echo $n 
-			./Static  -n $n -f $1>>StB.dat
-	done
-
 make MStatic 
-#echo StaticTreeBarrier with less cores>>Result.txt
-for n in $sample
-	do 
-		echo $n 
-			./MStatic -n $n -f $1>>MStB.dat
+
+for loop in $LOOP
+	do
+	echo Sense
+	for n in $sample
+		do 
+			echo $n 
+				./Sense  -n $n>>SeB$loop.dat
+		done
+	echo Msense
+	for n in $sample
+		do 
+			echo $n 
+				./MSense  -n $n>>MSeB$loop.dat
+		done
+	echo Tree
+	for n in $sample
+		do 
+			echo $n 
+				./Tree  -n $n -f $1>>TB$loop.dat
+		done
+	echo MTree
+	for n in $sample
+		do 
+			echo $n 
+				./MTree  -n $n -f $1>>MTB$loop.dat
+		done
+	echo Static
+	for n in $sample
+		do 
+			echo $n 
+				./Static  -n $n -f $1>>StB$loop.dat
+		done
+	echo MStatic
+	for n in $sample
+		do 
+			echo $n 
+				./MStatic -n $n -f $1>>MStB$loop.dat
+		done
+
+
 	done
 
 make clean
 
-gnuplot stressplot
+#gnuplot stressplot
 
 
 
